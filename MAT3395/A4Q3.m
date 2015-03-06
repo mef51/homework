e9ffce637cca7927d4f7d2b2ce302fe0aaa2cfb4
mef51 <hms.mohammed@gmail.com>
2015-03-06 17:00:59 -0500

    Models: finish the tumour question

--- MAT3395/A4Q3.m
+++ MAT3395/A4Q3.m
@@ -1,13 +1,15 @@
-
+% MAT3395
+% Mohammed Chamma, Stewart Clelland, Alex Commeau
+% Assignment 4 Question 3
 function y = A4Q3
     fprintf('===');
     % plot the logistic equation
     % dV/dt = rV(1-V/k) r = 2, k = 100
     V = [0:0.1:110];
-    r = 2; 
+    r = 2;
     k = 100;
     logistic = r * (V .* (1 - V/k));
-
+ 
     % plot the gompertz equation
     % dV/dt = V(a-b*ln(V))
     b = r;
@@ -19,18 +21,18 @@ function y = A4Q3
     ylabel('Derivative of V');
     title('Gompertz and Logistic Equation');
     legend('Gompertz Equation', 'Logistic Equation', 'Location', 'southeast');
-    
+   
     function dV = gompertzEq(t, V)
         dV = zeros(1,1);
         dV(1) = V(1)*(a - b*log(V(1)));
     end
-
+ 
     function dy = logisticEq(t, y)
         dy = zeros(1,1);
         dy(1) = r * y(1) * (1 - y(1)/k);
     end
-    
-    timespan=[0:0.01:4];   
+   
+    timespan=[0:0.01:4];  
     initial=[1];        
     [T, gompertzSol] = ode45(@gompertzEq,timespan,initial);
     [T2, logisticSol] = ode45(@logisticEq,timespan,initial);
